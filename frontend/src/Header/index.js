f0e76d78fd3e9ac846353d5bff59465cde10c956
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2022-05-12 20:15:55 -0500

    final tirht changes

--- frontend/src/Header/index.js
+++ frontend/src/Header/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import logo from '../home_images/tutorlogo2.png'
 import './index.css'
 function Header() {
     const navigate = useNavigate()
     return (
         <nav className="navbar navbarCustomStyle navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid p-0">
-                <img className="navbar-brand imgBorderCut" src={logo} width="250px" onClick={()=>{ navigate('/')}}/>
+                <img className="navbar-brand imgBorderCut" src={'/images/tutorlogo2.png'} width="250px" onClick={()=>{ navigate('/')}}/>
                 <button className="navbar-toggler customHamburger" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                     aria-label="Toggle navigation">
